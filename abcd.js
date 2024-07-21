ecbcbb79098d1f418965a397950ef1ea38c664f3
dhd2333 <107297718+dhd2333@users.noreply.github.com>
2024-07-21 13:33:04 +0800

    Update abcd.js

--- abcd.js
+++ abcd.js
@@ -1,3 +1,3 @@
 https://api.app.code-abc.com/api/v1/get/WasteCat/vip url script-response-body https://raw.githubusercontent.com/dhd2333/123/main/abc.js
-
+https://api.app.code-abc.com/api/v1/(add|get)/WasteCat/(statisticsAddUse|PhoneUpdate) script-request-header https://raw.githubusercontent.com/dhd2333/123/main/abcd.js
 hostname = api.app.code-abc.com
