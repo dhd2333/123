6262c0457fc2eaa926900d068d4c00928b340f96
dhd2333 <107297718+dhd2333@users.noreply.github.com>
2024-07-21 13:37:15 +0800

    Update abcd.js

--- abcd.js
+++ abcd.js
@@ -1,3 +1,6 @@
-https://api.app.code-abc.com/api/v1/get/WasteCat/vip url script-response-body https://raw.githubusercontent.com/dhd2333/123/main/abc.js
-https://api.app.code-abc.com/api/v1/(add|get)/WasteCat/(statisticsAddUse|PhoneUpdate) script-request-header https://raw.githubusercontent.com/dhd2333/123/main/abcd.js
+https://api.app.code-abc.com/api/v1/get/WasteCat/vip url script-response-body https://raw.githubusercontent.com/dhd2333/123/main/abcd.js
+https://api.app.code-abc.com/api/v1/add/WasteCat/statisticsAddUse script-request-header https://raw.githubusercontent.com/dhd2333/123/main/abcd.js
+https://api.app.code-abc.com/api/v1/get/WasteCat/PhoneUpdate script-request-header https://raw.githubusercontent.com/dhd2333/123/main/abcd.js
+
+
 hostname = api.app.code-abc.com
